8ed0d0ddd6db2fd3e9063a552a243fb89d1f0f7e
DursyArts <alphacheckde@gmail.com>
2020-02-23 17:04:46 +0100

    Create example.sql

---
+++ example.sql
@@ -0,0 +1,32 @@
+create table comments
+(
+    ID        int auto_increment
+        primary key,
+    thread_id int          null,
+    owner     varchar(255) null,
+    date      varchar(255) null,
+    content   text         null
+);
+
+create table threads
+(
+    ID      int auto_increment
+        primary key,
+    owner   varchar(255) null,
+    date    varchar(255) null,
+    content text         not null,
+    title   varchar(255) null
+);
+
+create table user
+(
+    ID       int auto_increment
+        primary key,
+    username varchar(255) null,
+    password varchar(255) null,
+    ip       varchar(255) null,
+    email    varchar(255) null,
+    role     varchar(255) null
+)
+    comment 'user table';
+
