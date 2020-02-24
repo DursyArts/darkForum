8a77f363ffcd1e76af5f51290228a3d5256edeb1
DursyArts <alphacheckde@gmail.com>
2020-02-24 02:26:22 +0100

    Update example.sql

--- example.sql
+++ example.sql
@@ -20,13 +20,16 @@ create table threads
 
 create table user
 (
-    ID       int auto_increment
+    ID           int auto_increment
         primary key,
-    username varchar(255) null,
-    password varchar(255) null,
-    ip       varchar(255) null,
-    email    varchar(255) null,
-    role     varchar(255) null
+    username     varchar(255) null,
+    password     varchar(255) null,
+    ip           varchar(255) null,
+    email        varchar(255) null,
+    role         varchar(255) null,
+    post_count   int          null,
+    thread_count int          null,
+    avatar       varchar(255) null
 )
     comment 'user table';
 
